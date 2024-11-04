a96924f1173266a813cc9ad68c0cfc4361675bf3
Leonardo <leonardomartinslopes2@gmail.com>
2024-11-04 13:16:56 -0300

    Adição funções cria e insere

--- tp5/fprio.c
+++ tp5/fprio.c
@@ -2,4 +2,45 @@
 // Carlos Maziero, DINF/UFPR, Out 2024
 // Implementação com lista encadeada simples
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "fprio.h"
+
 // A COMPLETAR
+
+struct fprio_t *fprio_cria (){
+    struct fprio_t *primeiro;
+    if (! (primeiro = malloc(sizeof(struct fprio_t))) );
+        return NULL;
+
+    primeiro->prim = NULL;
+    primeiro->num = 0;
+
+    return primeiro;
+}
+
+int fprio_insere (struct fprio_t *f, void *item, int tipo, int prio){
+    if (f == NULL || item == NULL)
+        return -1;
+    struct fpnodo_t *novo;
+    if (! (novo = mallco(sizeof(struct fpnodo_t))) );
+        return -1;
+
+    novo->item = item;
+    novo->prio = prio;
+    novo->prox = NULL;
+    novo->tipo = tipo;
+    //fila vazia
+    if (f->num == 0)
+    {
+        f->prim = novo;
+        f->num++;
+
+        return f->num;
+    }
+    
+    struct fpnodo_t *aux;
+    aux = f->prim;
+    while (prio > aux->prio || aux->prox != NULL)
+        aux = aux->prox;
+}
