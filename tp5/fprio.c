ea8eee4e9cdd21da9294880cfbe0cc17ef845d67
Leonardo <leonardomartinslopes2@gmail.com>
2024-11-04 22:21:44 -0300

    Adição função retira

--- tp5/fprio.c
+++ tp5/fprio.c
@@ -41,6 +41,69 @@ int fprio_insere (struct fprio_t *f, void *item, int tipo, int prio){
     
     struct fpnodo_t *aux;
     aux = f->prim;
-    while (prio > aux->prio || aux->prox != NULL)
+    //primeiro item da fila tem menos prioridade
+    if (aux->prio > prio)
+    {
+        novo->prox = aux;
+        f->prim = novo;
+        f->num++;
+
+        return f->num;
+    }
+    //caminha se a prioridade for menor
+    //para se chegar no ultimo item
+    while (prio > aux->prox->prio || aux->prox != NULL)
         aux = aux->prox;
+    //insere o item
+    novo->prox = aux->prox;
+    aux->prox = novo;
+    f->num++;
+
+    return f->num;
+}
+
+struct fprio_t *fprio_destroi (struct fprio_t *f){
+    struct fpnodo_t *aux;
+
+    if (f == NULL || f->num < 0)
+        return NULL;
+    
+    while (aux != NULL)
+    {
+        struct fpnodo_t *temp = aux;
+        //move para o proximo
+        aux = aux->prox;
+        //libera o atual
+        free (temp->item);
+        free (temp);
+        temp = NULL;
+    }
+
+    free (f);
+    f = NULL;
+
+    return NULL;
+}
+
+void *fprio_retira (struct fprio_t *f, int *tipo, int *prio){
+    if (f == NULL || tipo == NULL || prio == NULL)
+        return NULL;
+
+    if (f->num == 0)
+        return NULL;
+
+    struct fpnodo_t *aux;
+    aux = f->prim;
+    if (f->num == 1)
+    {
+        *tipo = aux->tipo;
+        *prio = aux->prio;
+        free (aux->item);
+        free (aux);
+        aux = NULL;
+        f->num--;
+
+        return;
+    }
+    f->prim = aux->prox;
 }
